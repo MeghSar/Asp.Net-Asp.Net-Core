d2b3f5b6bf458360595830a437c862a880f57d11
MeghSar <sarkarcrj@gmail.com>
2022-02-01 12:38:43 +0530

    Added

---
+++ .NetCoreProject/StudentCoreApp/StudentCoreApp/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentCoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace StudentCoreApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDbContext _studentDbContext;
+        public StudentController(StudentDbContext studentDbContext)
+        {
+            _studentDbContext = studentDbContext;
+
+        }
+       public IActionResult Index()
+        {
+            var studentList = _studentDbContext.Student.ToList();
+            return View(studentList);
+        }
+        public IActionResult StudentEntry()
+        {
+            return View();
+        }
+        [HttpPost]
+        
+        public IActionResult StudentEntry(Student student)
+        {
+            _studentDbContext.Student.Add(student);
+            _studentDbContext.SaveChanges();
+            ViewBag.message = "Student details saved automatically";
+            return View();
+        }
+        public IActionResult EditStudent(int Roll)
+        {
+            return View();
+        }
+        public IActionResult DeleteStudent(int Roll)
+        {
+            return View();
+        }
+    }
+}
---
+++ .NetCoreProject/StudentCoreApp/StudentCoreApp/Models/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentCoreApp.Models
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Student> Student { get; set; }
+        
+    }
+
+}
